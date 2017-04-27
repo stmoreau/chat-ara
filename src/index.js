8ad0cb8d47d3cc5aab4e8473e5d685a0ef2c8f8d
Stephane Moreau <st.moreau@outlook.com.gr>
2017-04-27 02:35:44 +0100

    add or remove my messages

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ import './index.css';
 
 import { startListeningToAuthChanges } from './actions/auth';
 import { startListeningForUsers } from './actions/users';
+import { startListeningForMessages } from './actions/messages';
 
 const middleware = [ thunk ];
 const enhancers = [];
@@ -26,6 +27,7 @@ const store = createStore(
 
 store.dispatch(startListeningToAuthChanges());
 store.dispatch(startListeningForUsers());
+store.dispatch(startListeningForMessages());
 
 ReactDOM.render(
   <Provider store={store}>
