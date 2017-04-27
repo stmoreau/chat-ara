e8984609f81e33a7a692bd7344532317965feff8
Stephane Moreau <st.moreau@outlook.com.gr>
2017-04-27 01:22:28 +0100

    google sign in and out with redux and firebase

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,14 +1,34 @@
+import { auth, googleAuthProvider } from '../firebase';
+
 export const signIn = () => {
-  return {
-    type: 'SIGN_IN',
-    email: 'bill@example.com',
-    displayName: 'Bill Murray',
-    photoURL: 'http://www.fillmurray.com/200/200',
-    uid: 'firstUser'
+  return (dispatch) => {
+    dispatch({ type: 'ATTEMPTING_LOGIN'});
+    auth.signInWithPopup(googleAuthProvider).then(({ user }) => {
+      dispatch(signedIn(user));
+    });
   };
 };
 
 export const signOut = () => {
+  return (dispatch) => {
+    dispatch({ type: 'ATTEMPTING_LOGIN'});
+    auth.signOut().then(() => {
+      dispatch(signedOut);
+    });
+  };
+};
+
+const signedIn = (user) => {
+  return {
+    type: 'SIGN_IN',
+    email: user.email,
+    displayName: user.displayName,
+    photoURL: user.photoURL,
+    uid: user.uid
+  };
+};
+
+const signedOut = () => {
   return {
     type: 'SIGN_OUT'
   };
